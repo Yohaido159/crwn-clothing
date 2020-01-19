b045550f86c886d7346aadc44bea3e8e1dfae8b0
yohai <yohaido159@gmail.com>
2020-01-19 14:02:04 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
 import React from "react";
 
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1> HATS PAGE</h1>
-    </div>
-  );
-};
 function App() {
-
   return (
     <div>
+      <Header/>
+
+      <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
-
 }
 
 export default App;
