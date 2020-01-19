78552a65be503136d8e8f14d20553c6fe7ed4cb5
yohai <yohaido159@gmail.com>
2020-01-19 22:16:40 +0200

    add register

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Header from "./components/header/header.component";
 
 import signInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
-import { auth } from "./firebase/firebase.utils";
+import { auth , createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -26,11 +26,26 @@ class App extends React.Component {
 
 
   componentDidMount() {
-    this.unsubscribeFormAuth =  auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-      console.log(user);
-    })
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+
+          console.log(this.state);
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
+
 
   componentWillUnmount() {
     this.unsubscribeFormAuth()
---
+++ src/firebase/test.js
@@ -0,0 +1,6 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+
+const firestore = firebase.firestore();
+
+firestore.collection("users").doc("QHdsSu4clB2neoofQhD0").collection("cartItem").doc("bfrefvF8esfiR9hLLX7V")
