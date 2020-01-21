58a4e4d1e155632b80993e2706b89c1f234e8e93
yohai <yohaido159@gmail.com>
2020-01-21 22:07:12 +0200

    cart-icon and dropdown

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./cart-dropdown.styles.scss";
+
+import CustumButton from "../custom-button/custom-button.component";
+
+const CartDropdown = () => (
+    <div className="cart-dropdown">
+        <div className="cart-item"></div>
+        <CustumButton >GO TO CHECKOUT</CustumButton>
+
+    </div>
+)
+
+export default CartDropdown;
